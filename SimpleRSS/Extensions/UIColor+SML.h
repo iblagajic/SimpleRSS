2442950da51a74b53fd9d2e7e18cb82e69bb56bd
Ivan Blagajic <iblagajic@me.com>
2014-07-23 11:17:23 +0200

    UI improvements Fix core data errors Rename files (ViewController -> TableViewController)

---
+++ SimpleRSS/Extensions/UIColor+SML.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+SML.h
+//  SimpleRSS
+//
+//  Created by Ivan Blagajić on 04/07/14.
+//  Copyright (c) 2014 Simple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (SML)
+
+@end
---
+++ SimpleRSS/Extensions/UIColor+SML.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+SML.m
+//  SimpleRSS
+//
+//  Created by Ivan Blagajić on 04/07/14.
+//  Copyright (c) 2014 Simple. All rights reserved.
+//
+
+#import "UIColor+SML.h"
+
+@implementation UIColor (SML)
+
+@end
